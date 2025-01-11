887b0f006b1f23696bd9faabfeabcaafd857a0ea
suhyeon lee <tngus0673@naver.com>
2025-01-11 22:26:45 +0900

    docs: 초기 세팅

---
+++ LastCoffee/LastCoffee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LastCoffee
+//
+//  Created by 이수현 on 1/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
