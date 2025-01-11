476dd4f94f5e37bb4b90745934b585d33a9ba126
minyoy <65756020+minyoy@users.noreply.github.com>
2025-01-12 03:27:13 +0900

    feat/#20: 기록 추가 화면 구현

---
+++ LastCoffee/LastCoffee/ViewControllers/AddNoteViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AddNoteViewController.swift
+//  LastCoffee
+//
+//  Created by 주민영 on 1/12/25.
+//
+
+import UIKit
+
+class AddNoteViewController: UIViewController {
+    public var receivedData: NoteSearchModel?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view = addNoteView
+        self.navigationController?.isNavigationBarHidden = false
+        self.navigationItem.title = "새 기록"
+        
+        if let data = receivedData {
+            addNoteView.selectedCoffee.text = data.name
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    private lazy var addNoteView: AddNoteView = {
+        let view = AddNoteView()
+        return view
+    }()
+
+}
+
