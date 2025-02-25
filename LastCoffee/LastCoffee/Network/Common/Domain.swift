fcffcae527ee58cb0b0210777e980af8efd23aa6
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-02-25 10:24:51 +0900

    refactoring/#68 도메인 수정 및 폴더 정리

---
+++ LastCoffee/LastCoffee/Network/Common/Domain.swift
@@ -0,0 +1,10 @@
+// Copyright © 2024 DRINKIG. All rights reserved
+
+import UIKit
+
+public struct API {
+    public static let baseURL = "https://www.lastcoffee.shop/"
+    public static let authURL = "\(baseURL)/auth"
+    public static let noteURL = "\(baseURL)/note"
+    public static let coffeeURL = "\(baseURL)/coffee"
+}
