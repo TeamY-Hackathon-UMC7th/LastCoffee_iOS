3dbd5d32c99be21e7c48b944881df054e0ba9634
suhyeon lee <tngus0673@naver.com>
2025-02-26 11:15:03 +0900

    refactor/#72: 홈뷰 오토레이아웃 리팩토링

--- LastCoffee/LastCoffee/Util/Constants.swift
+++ LastCoffee/LastCoffee/Util/Constants.swift
@@ -8,3 +8,7 @@
 import Foundation
 import UIKit
 
+public struct Constants {
+    // SE 기기일 떄 바텀의 safeAreaLayoutGuide가 탭바 높이보다 아래에 있어서 추가 패딩이 필요함
+    public static var seBottomExtraPadding: CGFloat = 30
+}
